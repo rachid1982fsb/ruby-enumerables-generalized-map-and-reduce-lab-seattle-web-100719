33f419d9fb84068034e1bef9c4a2dec5d4f02937
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-30 03:27:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new << (yield (source_array[i]))
+    i += 1
+  end
+  return  new
+end
+
+#def reduce 
+ 
+ 
+ p map([2,4,6,1]){|i| i*i}
